2e50ea92bd1e8b58bfa384b16564c1c397b204ef
Kate-Mil <114938475+Kate-Mil@users.noreply.github.com>
2023-05-10 13:06:15 +0300

    Initial commit

---
+++ src/components/ContactList/ContactList.js
@@ -0,0 +1,28 @@
+import css from './ContactList.module.css';
+import PropTypes from 'prop-types';
+import ContactItem from '../ContactItem/ContactItem';
+
+export default function ContactList({ visibleContacts, onDeleteContact }) {
+  return (
+    <ul className={css.contact__list}>
+      {visibleContacts.map(({ id, name, number }) => (
+        <ContactItem
+          id={id}
+          key={id}
+          name={name}
+          number={number}
+          onDeleteContact={onDeleteContact}
+        />
+      ))}
+    </ul>
+  );
+}
+
+ContactList.propTypes = {
+  visibleContacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  onDeleteContact: PropTypes.func.isRequired,
+};
